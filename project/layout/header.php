3d5d8c7aeda09d873b603ac6e4d00188bbb2a9f3
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-08-14 21:34:52 -0400

    add: footer

---
+++ project/layout/header.php
@@ -0,0 +1,14 @@
+<header class="text-gray-600 body-font px-8">
+    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a href="index.php" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+            </svg>
+            <span class="ml-3 text-3xl">Read<span class="text-blue-600">Wise</span></span>
+        </a>
+        <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <a href="signin.php" class="mr-5 hover:text-gray-900 hover:font-semibold">Sign In</a>
+            <a href="signup.php" class="mr-5 hover:text-gray-900 hover:font-semibold">Sign Up</a>
+        </nav>
+    </div>
+</header>
