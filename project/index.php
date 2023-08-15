6d024b87906e3617ed61fbc0ef52239b712eb0fb
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-08-14 23:25:48 -0400

    fix: homepage styles

--- project/index.php
+++ project/index.php
@@ -5,13 +5,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Home - ReadWise</title>
         <script src="https://cdn.tailwindcss.com"></script>
-        <?php
-            require ('layout/header.php');
-        ?>
     </head>
-    <body>
-        <section class="section w-full space-4 px-8 py-12 bg-white">
-            <form class="flex items-center h-[55vh] md:h-[75vh] lg:h-[72vh] justify-center px-4 md:px-12">
+    <body class="h-screen">
+        <!-- Header/Navbar -->
+        <section class="h-[10vh] bg-red-300"><?php require ('layout/header.php'); ?></section>
+        <!-- Main Section -->
+        <section class="h-[80vh] w-full space-4 px-8 py-12 bg-white">
+            <form class="flex items-center h-full justify-center px-4 md:px-12">
                 <div class="relative w-full md:w-3/4 lg:w-1/2">
                     <input type="search" id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Search Books" required>
                     <button type="submit" class="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
@@ -23,8 +23,7 @@
                 </div>
             </form>
         </section>
-        <?php
-            require ('layout/footer.php');
-        ?>
+        <!-- Footer -->
+        <section class="h-[10vh]"><?php require ('layout/footer.php'); ?></section>
     </body>
 </html>
