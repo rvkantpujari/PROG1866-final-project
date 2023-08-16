591785828410b94efc70ce5afee77c6ccfd1f79b
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-08-15 20:41:42 -0400

    fix: Add Book

--- project/addBook.php
+++ project/addBook.php
@@ -3,18 +3,40 @@
     
     session_start();
 
+    $db = new DB;
     $authors = $db->table("authors")->selectAll();
     $publishers = $db->table("publishers")->selectAll();
 
     // Redirect if user logged in
-    if(isset($_SESSION['loggedIn'])) {
+    if(!isset($_SESSION['loggedIn'])) {
         echo "<script>window.location.href='index.php';</script>";
     }
     
     // If add book button is clicked
     if(isset($_POST['btnAddBook'])) 
     {
+        function generateISBN() 
+        {
+          $prefix = '978'; // Common prefix for books
+          // Generate 9 random digits
+          $randomDigits = '';
+          for ($i = 0; $i < 9; $i++) {
+              $randomDigits .= mt_rand(0, 9);
+          }
+          // Calculate checksum digit using ISBN-13 formula
+          $isbnDigits = $prefix . $randomDigits;
+          $checksum = 0;
+          for ($i = 0; $i < 12; $i++) {
+              $checksum += ($i % 2 === 0) ? intval($isbnDigits[$i]) : 3 * intval($isbnDigits[$i]);
+          }
+          $checksum = (10 - ($checksum % 10)) % 10;
+          // Combine all parts to form the ISBN
+          $isbn = $isbnDigits . $checksum;
+          return $isbn;
+        }
+        
         // Assign data from POST request
+        $generatedISBN = generateISBN();
         $title = $_POST['title'];
         $description = $_POST['description'];
         $edition = $_POST['edition'];
@@ -26,12 +48,9 @@
         $db = new DB;
 
         // Insert user data in order to create new book
-        $result = $db->table('books')->insert(['book_title', 'book_desc', 'book_edition', 'book_published_year', 'book_author', 'book_publisher'], array($title, $description, $edition, $year, $author, $publisher), 'ssssss');
+        $result = $db->table('books')->insert(['book_isbn', 'book_title', 'book_desc', 'book_edition', 'book_published_year', 'book_author', 'book_publisher'], array($generatedISBN, $title, $description, $edition, $year, $author, $publisher), 'issssss');
         
         if($result) {
-            $_SESSION['loggedIn'] = 1;
-            $_SESSION['user'] = str_contains($email, 'admin') ? 'admin' : 'user';
-            $_SESSION['user_email'] = $email;
             echo "<script>window.location.href='dashboard.php';</script>";
         }
     }
@@ -65,33 +84,33 @@
                     <form method="post" class="my-8 grid grid-cols-12 gap-x-2">
                         <div class="col-span-full mt-1">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">Title</label>
-                            <input class="block w-full px-4 py-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="text" name="title" placeholder="Book Title" />
+                            <input class="block w-full px-4 py-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="text" name="title" placeholder="Book Title" required />
                         </div>
 
                         <div class="col-span-full mt-3">
                           <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">Description</label>
-                          <textarea id="description" rows="4" class="block p-2.5 w-full text-gray-700 placeholder-gray-500 bg-white rounded-lg border border focus:border-blue-400 focus:ring-blue-400 focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" name="description" placeholder="Write your description here..."></textarea>
+                          <textarea required id="description" rows="4" class="block p-2.5 w-full text-gray-700 placeholder-gray-500 bg-white rounded-lg border border focus:border-blue-400 focus:ring-blue-400 focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" name="description" placeholder="Write your description here..."></textarea>
                         </div>
 
                         <div class="col-span-full md:col-span-6 mt-3">
                           <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">Edition</label>
-                          <input class="block w-full px-4 py-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="text" name="edition" placeholder="Book edition" />
+                          <input required class="block w-full px-4 py-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="text" name="edition" placeholder="Book edition" />
                         </div>
                         
                         <div class="col-span-full md:col-span-6 mt-3">
                           <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">Year</label>
-                          <input class="block w-full px-4 py-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="number" name="year" placeholder="Published year" />
+                          <input required class="block w-full px-4 py-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="number" name="year" placeholder="Published year" />
                         </div>
 
                         <div class="col-span-full mt-3">
                           <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">Author</label>
                           <div class="relative">
                             <select class="block w-full px-4 py-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" id="author" name="author">
-                              <?php foreach ($this->authors as $row): ?>
+                              <?php foreach ($authors as $row) { ?>
                                 <option value="<?php echo htmlspecialchars($row['author_email']); ?>">
-                                    <?php echo htmlspecialchars($row['author_fname']) + ' ' + htmlspecialchars($row['author_lname']); ?>
+                                    <?php echo htmlspecialchars($row['author_fname']) . ' ' . htmlspecialchars($row['author_lname']); ?>
                                 </option>
-                              <?php endforeach; ?>
+                              <?php } ?>
                             </select>
                           </div>
                         </div>
@@ -100,11 +119,11 @@
                           <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">Publishers</label>
                           <div class="relative">
                             <select class="block w-full px-4 py-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" id="publisher" name="publisher">
-                              <?php foreach ($this->publishers as $row): ?>
+                              <?php foreach ($publishers as $row) { ?>
                                 <option value="<?php echo htmlspecialchars($row['publisher_email']); ?>">
                                     <?php echo htmlspecialchars($row['publisher_name']); ?>
                                 </option>
-                              <?php endforeach; ?>
+                              <?php } ?>
                             </select>
                           </div>
                         </div>
