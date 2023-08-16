ca7ff10fd7f4677140c246f8e342c9e38d9f6f20
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-08-16 19:07:04 -0400

    fix: updated Schema

--- docs/schema.sql
+++ docs/schema.sql
@@ -1,35 +1,27 @@
--- Create DATABASE
 CREATE DATABASE IF NOT EXISTS readwise;
+USE readwise;
 
-
--- Create users table - It stores users' personal as well as login data
 CREATE TABLE IF NOT EXISTS readwise.users (
     user_email VARCHAR(100) PRIMARY KEY,
-    user_fname VARCHAR(50) NOT NULL,
+	user_fname VARCHAR(50) NOT NULL,
     user_lname VARCHAR(50) NOT NULL,
-    date_of_birth DATE,
+    date_of_birth DATE NOT NULL,
     password VARCHAR(100) NOT NULL
 );
 
-
--- Create authors table - It stores authors' personal data
 CREATE TABLE IF NOT EXISTS readwise.authors (
-    author_email VARCHAR(100) PRIMARY KEY,
-    author_fname VARCHAR(50) NOT NULL,
+	author_email VARCHAR(100) PRIMARY KEY,
+	author_fname VARCHAR(50) NOT NULL,
     author_lname VARCHAR(50) NOT NULL
 );
 
-
--- Create publishers table - It stores publishers' personal data
 CREATE TABLE IF NOT EXISTS readwise.publishers (
 	publisher_email VARCHAR(100) PRIMARY KEY,
 	publisher_name VARCHAR(200) NOT NULL
 );
 
-
--- Create books table - It stores Books' data such as ISBN, Title, Description, Edition etc.
 CREATE TABLE IF NOT EXISTS readwise.books (
-    book_isbn INT(13) PRIMARY KEY,
+	book_isbn INT(13) PRIMARY KEY,
     book_title VARCHAR(100) NOT NULL,
     book_desc TEXT,
     book_edition VARCHAR(50),
@@ -40,10 +32,8 @@ CREATE TABLE IF NOT EXISTS readwise.books (
     FOREIGN KEY (book_publisher) REFERENCES readwise.publishers (publisher_email) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
-
--- Create read table - It stores when a user started reading book from given list of books
-CREATE TABLE IF NOT EXISTS readwise.read(
-    book_isbn INT(13) NOT NULL,
+CREATE TABLE IF NOT EXISTS readwise.book_read(
+	book_isbn INT(13) NOT NULL,
     user_email VARCHAR(100) NOT NULL,
     read_on DATE NOT NULL DEFAULT NOW(),
     FOREIGN KEY (book_isbn) REFERENCES readwise.books (book_isbn) ON UPDATE CASCADE ON DELETE RESTRICT,
